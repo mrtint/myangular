09ddca53d2107e59f1bbf930d51e73404015fb55
Mr. Tint <mrtint0729@gmail.com>
2015-12-02 16:54:58 +0900

    make a new directive.
    1. a modal reused by javascript function.
    2. create modal directive.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,41 @@
 var myVideo = angular.module('myVideo', [])
 
+myVideo.directive('videoThumbnail', function() {
+	return {
+		restrict : 'E',
+		templateUrl : '/directives/video-thumbnail.html'
+	}
+})
+
+myVideo.directive('videoDetail', function() {
+	return {
+		restrict : 'E',
+		templateUrl : '/directives/video-detail.html'
+	}
+})
+
 myVideo.controller('PlayListController', ['$http', '$scope', '$sce', function($http, $scope, $sce){
 	var playList = this
 	playList.videos = []
-
-	$scope.test = function(videoId) {
-		var src = "http://www.youtube.com/embed/" + videoId + "?rel=0"
-		return $sce.trustAsResourceUrl(src)
-	}
 
 	$http.get('/datas/videos.json').success(function(data){
 		playList.videos = data.result
 	})
 
+	$scope.viewDetails = function(index) {
+		if(playList.videos.length > 0) {
+			var modalArea = $('#videoModal')
+			var videoData = playList.videos[index]
+
+			// stop video when we
+			modalArea.on('hide.bs.modal', function(e){
+				modalArea.find('.video-frame').attr('src', '')
+			})
+
+			modalArea.find('.modal-title').text(videoData.snippet.title)
+			modalArea.find('.video-desc').text(videoData.snippet.description)
+			modalArea.find('.video-frame').attr('src', 'http://www.youtube.com/embed/' + videoData.snippet.resourceId.videoId);
+			modalArea.modal('show')
+		}
+	}
 }])
---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(function(){
+	
+	$('h4.link-button').click(function(){
+		console.log("hi")
+		console.log($(this).attr('data-index'))
+	})
+})
